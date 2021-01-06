11caa7910f95d260928a556dad9dd8d61b09c917
Faizan Atif <faizanbutt833@gmail.com>
2021-01-06 18:34:10 +0500

    deployed

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var InventoryRegister = require('./models/inventoryregister');
 var Issue = require('./models/issue')
 var app = express();
 
-mongoose.connect("mongodb://localhost/nhmp", { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect("mongodb://localhost/nhmp", { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect("mongodb+srv://admin:admin33@cluster0.rbe1d.mongodb.net/nhmp?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
